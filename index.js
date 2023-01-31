0b9fb542958eb19ce70e11af0cedabfc01e165ce
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-01-30 23:53:24 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+$("#btn").click(function () {
+  //   $("#div1").show();
+  //   $("#div1").hide();
+  //   $("#div1").hide(2000).show(2000);
+  //   $("#div1").toggle();
+  //   $("#div1").fadeOut(2000);
+  //   $("#div1").fadeIn(2000);
+  //   $("#div1").fadeOut(2000).fadeIn(2000);
+  //   $("#div1").fadeToggle(2000);
+  //   $("#div1").fadeTo(2000, 0.4);
+  //   $("#div1").slideUp(2000);
+  //   $("#div1").slideDown(2000);
+  //   $("#div1").slideUp(2000).slideDown(2000);
+  $("#div1").slideToggle(2000);
+});
